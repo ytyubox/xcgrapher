42e702e9f78f63dfa5c67e4d0f9b95bb21019be7
ytyubox <ytyubox@gmail.com>
2023-04-12 11:44:51 +0800

    import SomeAppCore

---
+++ Tests/SampleProjects/SomeAppWithLocalPackage/SomeApp/Imports/Local/LocalImports.swift
@@ -0,0 +1,9 @@
+//
+//  SomeAppCoreImports.swift
+//  SomeApp
+//
+//  Created by Yu Yu on 2023/4/12.
+//
+
+import Foundation
+import SomeAppCore
--- Tests/SampleProjects/SomeAppWithLocalPackage/SomeAppCore/Sources/SomeAppCore/SomeAppCore.swift
+++ Tests/SampleProjects/SomeAppWithLocalPackage/SomeAppCore/Sources/SomeAppCore/SomeAppCore.swift
@@ -1,2 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+public struct SomeType {
+  public init() {}
+  public var value = 42
+}
